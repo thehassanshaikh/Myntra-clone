9f4eca23d1247fcf835eb6004074ee478bfb1c0d
thehassanshaikh <thehasanshaikh@gmail.com>
2023-03-21 20:27:23 +0530

    Created nav bar (Header)

---
+++ myntra/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import "./Navbar.css"
+import { IoPersonOutline, IoBagHandleOutline } from "react-icons/io5";
+import { BsBookmarks } from "react-icons/bs";
+
+export const Navbar = ()=>{
+    return (
+        <>
+        <header>
+            <div className="logo-container">
+                <img className="logo" src="https://ww1.freelogovectors.net/wp-content/uploads/2023/01/myntra-logo-freelogovectors.net_-400x225.png" alt="Myntra logo"/>
+            </div>
+            <nav className="nav-container">
+                <ul className="nav-items">
+                   <li className="nav-item">Men</li>
+                   <li className="nav-item">Women</li>
+                   <li className="nav-item">Kids</li>
+                   <li className="nav-item">Home & living</li>
+                   <li className="nav-item">Stdio</li>
+                </ul>
+            </nav>
+            <div className="search-bar-container">
+                <input className="search-bar" type="text" placeholder="Seach for porducts,Brands or more" />
+            </div>
+            <div className="account-item-container">
+                <ul className="accounts-items">
+                    <li className="account-item"><IoPersonOutline/> Profile</li>
+                    <li className="account-item"><BsBookmarks/>Wishlist</li>
+                    <li className="account-item"><IoBagHandleOutline/> Profile</li>
+                    
+                </ul>
+            </div>
+
+        </header>
+        </>
+    )
+}
