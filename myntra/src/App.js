9f75e7ed91e6ab70173b5868d7f796f27c515613
thehassanshaikh <thehasanshaikh@gmail.com>
2023-03-22 20:00:22 +0530

    Created Sidebar

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,7 +1,12 @@
 import { Navbar } from "./components/Navbar/Navbar";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 function App() {
   return (
-   <Navbar />
+    <>
+    <Navbar />
+    <Sidebar />
+    </>
+   
   );
 }
 
---
+++ myntra/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import "./Sidebar.css"
+export const Sidebar = () => {
+    return(
+        <aside className="sidebar-container">
+            <div className="filter-holder">
+                {/* Brands category */}
+                <div className="brands">
+                    <h3 className="sidebar-lable">Brands</h3>
+                    <ul>
+                        <li className="category-items"><input type="checkbox" />Roadstar</li>
+                        <li className="category-items"><input type="checkbox" />Wrong</li>
+                        <li className="category-items"><input type="checkbox" />Nike</li>
+                        <li className="category-items"><input type="checkbox" />Lives</li>
+                    </ul>   
+                </div>
+                {/* Price Category */}
+                <div className="price">
+                    <h3 className="sidebar-lable">Prices</h3>
+                    <ul>
+                        <li className="category-items"><input type="checkbox" />150 - 499</li>
+                        <li className="category-items"><input type="checkbox" />500 - 799</li>
+                        <li className="category-items"><input type="checkbox" />800 - 999</li>
+                        <li className="category-items"><input type="checkbox" />1000 +</li>
+                    </ul>   
+                </div>
+                {/* Gender Category */}
+                <div className="gender">
+                    <h3 className="sidebar-lable">Gender</h3>
+                    <ul>
+                        <input type="radio" id="male" name="gender" value={"male"} />
+                        <label htmlFor="male">Male</label>
+                         <br/>
+                         <input type="radio" id="female" name="gender" value={"female"} />
+                         <label htmlFor="male">Female</label>
+                    </ul>
+                    
+                </div>
+            </div>
+        </aside>
+    )
+}
