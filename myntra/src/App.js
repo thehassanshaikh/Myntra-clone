d25cfebaa22efa933359dbf54fd149723b1a392a
thehassanshaikh <thehasanshaikh@gmail.com>
2023-03-22 20:27:20 +0530

    Created mens page

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,10 +1,15 @@
 import { Navbar } from "./components/Navbar/Navbar";
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Mens } from "./components/Mens/Mens";
 function App() {
   return (
     <>
     <Navbar />
+    <div>
     <Sidebar />
+    <Mens />
+    </div>
+    
     </>
    
   );
---
+++ myntra/src/components/Mens/Mens.jsx
@@ -0,0 +1,10 @@
+import "./Mens.css"
+export const Mens = () => {
+    return(
+        <section className="mens-container">
+            <h1>mens page</h1>
+        </section>
+        
+        
+    );
+}
