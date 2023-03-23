26e72cb15c4981f1fd3b6acdd5ef2873fc1bcd92
thehassanshaikh <thehasanshaikh@gmail.com>
2023-03-23 20:28:07 +0530

    removed the men component and added context

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,13 +1,13 @@
+import { Context } from "./components/Context/Context";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Sidebar } from "./components/Sidebar/Sidebar";
-import { Mens } from "./components/Mens/Mens";
 function App() {
   return (
     <>
     <Navbar />
     <div>
     <Sidebar />
-    <Mens />
+    <Context/>
     </div>
     
     </>
---
+++ myntra/src/components/Context/Context.js
@@ -0,0 +1,6 @@
+
+export const Context = () => {
+    return (
+        <h1>Content APi</h1>
+    )
+}
