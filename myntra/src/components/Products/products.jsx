c38f3c8537e670874717c4455d7c8c5e8c87eee0
Mohammad Hassan shaikh <thehasanshaikh@gmail.com>
2023-03-25 14:44:48 +0530

    added product page content and style

--- myntra/src/components/Products/products.jsx
+++ myntra/src/components/Products/products.jsx
@@ -3,60 +3,32 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { ApiDataContext } from "../../App"
 import "./products.css"
 
-export const Products = ()=>{
+export const Products = () => {
 
-const data = useContext(ApiDataContext);
+    const data = useContext(ApiDataContext);
 
-return(
-<section className="products-section">
-    <div className="product-container">
-        <div className="product-grid">
-            <img src="https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg" alt="product image"
-                className="pro-img" />
-            <div className="brand-con">
-                <h4 className="brand-name">Brand Name</h4>
-                <AiOutlineHeart />
-            </div>
-            <h3 className="pro-name">Men Rust Red Graphic Printed Cotton</h3>
-            <h3 className="pro-price">$2,155</h3>
-            <button className="pro-buttton">Add to Bag</button>
-        </div>
-        <div className="product-grid">
-            <img src="https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg" alt="product image"
-                className="pro-img" />
-            <div className="brand-con">
-                <h4 className="brand-name">Brand Name</h4>
-                <AiOutlineHeart />
-            </div>
-            <h3 className="pro-name">Men Rust Red Graphic Printed Cotton</h3>
-            <h3 className="pro-price">$2,155</h3>
-            <button className="pro-buttton">Add to Bag</button>
-        </div>
-        <div className="product-grid">
-            <img src="https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg" alt="product image"
-                className="pro-img" />
-            <div className="brand-con">
-                <h4 className="brand-name">Brand Name</h4>
-                <AiOutlineHeart />
-            </div>
-            <h3 className="pro-name">Men Rust Red Graphic Printed Cotton</h3>
-            <h3 className="pro-price">$2,155</h3>
-            <button className="pro-buttton">Add to Bag</button>
-        </div>
-        <div className="product-grid">
-            <img src="https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg" alt="product image"
-                className="pro-img" />
-            <div className="brand-con">
-                <h4 className="brand-name">Brand Name</h4>
-                <AiOutlineHeart />
-            </div>
-            <h3 className="pro-name">Men Rust Red Graphic Printed Cotton</h3>
-            <h3 className="pro-price">$2,155</h3>
-            <button className="pro-buttton">Add to Bag</button>
-        </div>
-    </div>
+    return (
+        <section className="products-section">
+            <div className="product-container">
+                {data.map(item => (
+                    <div className="product-grid">
+                        <div className="pro-img-con">
+                            <img src={item.image} alt="product image"
+                                className="pro-img" />
+                        </div>
+                        <div className="product-ditails">
+                            <h3 className="product-brand">Brand Name</h3>
+                            <h3 className="product-title">{item.title}</h3>
+                            <h3 className="product-price">₹ {item.price}</h3>
+                            <button className="product-button">View More</button>
+                        </div>
 
-</section>
+                    </div>
+                ))}
 
-)
+            </div>
+
+        </section>
+
+    )
 }
